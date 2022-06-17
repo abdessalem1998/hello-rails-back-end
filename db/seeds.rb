1d1e28cff139fcfe8bea3039d6f851a6c6dba12d
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-06-18 00:25:28 +0100

    Add seeds and Add validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Greeting.destroy_all
+
+Greeting.create(message: 'message 1')
+Greeting.create(message: 'message 2')
+Greeting.create(message: 'message 3')
+Greeting.create(message: 'message 4')
+Greeting.create(message: 'message 5')
